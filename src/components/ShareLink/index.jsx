5623331c60a6fc38f1e3886154d681dd5636638b
Azimka <71658005+Azimka228@users.noreply.github.com>
2023-09-07 19:10:21 +0300

    feat: added shared Link

--- src/components/ShareLink/index.jsx
+++ src/components/ShareLink/index.jsx
@@ -1,12 +1,24 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {useToggleBodyOverflow} from "../../hooks/useToggleBodyOverflow";
 import shareIcon from '../../icons/share.png'
 import cn from "classnames";
 
+
+// Import Swiper styles
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
+
+
 import "./ShareLink.css"
+import {Swiper, SwiperSlide} from "swiper/react";
 
 export const ShareLink = () => {
  const [isOpen, setIsOpen] = useState(false)
+ const [disablingSlideBtns, setDisablingSlideBtns] = useState({
+  next: false,
+  prev: true
+ })
+ const swiperRef = useRef()
  const clipboardUrl = "https://www.stickystick.com/some/url"
 
  useToggleBodyOverflow(isOpen)
@@ -22,6 +34,23 @@ export const ShareLink = () => {
  const handleCopyToClipBoard = () => {
   navigator.clipboard.writeText(clipboardUrl)
  }
+
+ const handleClickNextButton = () => {
+  swiperRef.current?.slideNext()
+  console.log(swiperRef.current)
+  setDisablingSlideBtns({prev: swiperRef.current.isBeginning, next: swiperRef.current.isEnd})
+ }
+
+ const handleClickPrevButton = () => {
+  swiperRef.current?.slidePrev()
+  console.log(swiperRef.current)
+  setDisablingSlideBtns({prev: swiperRef.current.isBeginning, next: swiperRef.current.isEnd})
+ }
+
+ const handleMoveSlide = (e) => {
+  setDisablingSlideBtns({prev: e.isBeginning, next: e.isEnd})
+ }
+
 
  const modalStyles = cn("shareLink__modal", {["shareLink__modal_opened"]: isOpen})
 
@@ -44,17 +73,54 @@ export const ShareLink = () => {
       <button onClick={handleClose} className="shareLink__button shareLink__button_close">X</button>
      </div>
      <div className="shareLink__content__text">
+      {!disablingSlideBtns.prev && <div onClick={handleClickPrevButton}
+                                        className="shareLink__content__slideBtn shareLink__content__slideBtn_left">{"<"}</div>}
       <div className="shareLink__contentHeader">
-       {item}
-       {item}
-       {item}
-       {item}
-       {item}
-       {item}
+       <Swiper
+        slidesPerView={5}
+        onSliderFirstMove={handleMoveSlide}
+        pagination={{
+         clickable: true,
+        }}
+        onBeforeInit={swiper => {
+         swiperRef.current = swiper
+        }}
+        className="mySwiper"
+        breakpoints={{// when window width is >= 320px
+         200: {
+          slidesPerView: 2,
+          spaceBetween: 10
+         },
+         300: {
+          slidesPerView: 3,
+          spaceBetween: 10
+         },
+         440: {
+          slidesPerView: 4,
+          spaceBetween: 10
+         }
+        }
+        }
+
+       >
+        <SwiperSlide>{item}</SwiperSlide>
+        <SwiperSlide>{item}</SwiperSlide>
+        <SwiperSlide>{item}</SwiperSlide>
+        <SwiperSlide>{item}</SwiperSlide>
+        <SwiperSlide>{item}</SwiperSlide>
+        <SwiperSlide>{item}</SwiperSlide>
+        <SwiperSlide>{item}</SwiperSlide>
+        <SwiperSlide>{item}</SwiperSlide>
+        <SwiperSlide>{item}</SwiperSlide>
+
+       </Swiper>
       </div>
+      {!disablingSlideBtns.next && <div onClick={handleClickNextButton}
+                                        className="shareLink__content__slideBtn shareLink__content__slideBtn_right">{">"}</div>}
+
      </div>
      <div className="shareLink__content__url">
-      <div>
+      <div className="shareLink__content__urlText">
        {clipboardUrl}
       </div>
       <button onClick={handleCopyToClipBoard} className="shareLink__content__urlButton">Копировать</button>
